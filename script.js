66d090889b7952ff7d7d043e43f966bc71537064
Kamil Dobosz <kamdobosz26@wp.pl>
2022-01-04 13:55:04 +0100

    commit

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 const squares = [...document.querySelectorAll("div")];
+const button = document.querySelector("button");
 const cardsColor = [
-  "red",
-  "red",
-  "blue",
-  "blue",
-  "green",
-  "green",
-  "pink",
-  "pink",
-  "grey",
-  "grey",
-  "yellow",
-  "yellow",
-  "gold",
-  "gold",
-  "brown",
-  "brown",
+    "red",
+    "red",
+    "blue",
+    "blue",
+    "green",
+    "green",
+    "pink",
+    "pink",
+    "violet",
+    "violet",
+    "yellow",
+    "yellow",
+    "gold",
+    "gold",
+    "brown",
+    "brown",
 ];
 
 let activeCard = "";
@@ -24,53 +25,51 @@ const activeCards = [];
 const pairs = cardsColor.length / 2;
 let gameResult = 0;
 
-showColor = function () {
-  activeCard = this;
-  activeCard.classList.remove("hidden");
+const showColor = function() {
+    activeCard = this;
+    if (activeCard == activeCards[0]) return;
+    activeCard.classList.remove("hidden");
 
-  if (activeCards.length === 0) {
-    activeCards[0] = this;
-    return;
-  } else {
-    squares.forEach((square) => square.removeEventListener("click", showColor));
-    activeCards[1] = this;
+    if (activeCards.length === 0) {
+        activeCards[0] = activeCard;
+    } else {
+        squares.forEach((square) => square.removeEventListener("click", showColor));
+        activeCards[1] = activeCard;
 
-    setTimeout(function () {
-      if (activeCards[0].className != activeCards[1].className) {
-        activeCards.forEach((card) => card.classList.add("hidden"));
-      } else {
-        activeCards.forEach((card) => card.classList.add("off"));
-        ++gameResult;
-        console.log(gameResult);
-        if (gameResult === pairs) {
-          alert("Wygrana!");
-        }
-      }
-
-      activeCards.length = 0;
-      activeCard = "";
-      squares.forEach((square) => {
-        square.addEventListener("click", showColor);
-      });
-    }, 1000);
-  }
+        setTimeout(function() {
+            if (activeCards[0].className === activeCards[1].className) {
+                activeCards.forEach((card) => card.classList.add("off"));
+                gameResult++;
+                if (gameResult == pairs) {
+                    alert("Wygrana!");
+                }
+            } else {
+                activeCards.forEach((card) => card.classList.add("hidden"));
+            }
+            activeCards.length = 0;
+            activeCard = "";
+            squares.forEach((square) => {
+                square.addEventListener("click", showColor);
+            });
+        }, 1000);
+    }
 };
 
-startGame = function () {
-  squares.forEach((square) => {
-    let number = Math.floor(Math.random() * cardsColor.length);
-
-    let color = cardsColor[number];
-    square.classList.add(color);
-    cardsColor.splice(number, 1);
-  });
-
-  setTimeout(function () {
+startGame = function() {
     squares.forEach((square) => {
-      square.classList.add("hidden");
-      square.addEventListener("click", showColor);
+        let number = Math.floor(Math.random() * cardsColor.length);
+
+        let color = cardsColor[number];
+        square.classList.add(color);
+        cardsColor.splice(number, 1);
     });
-  }, 2000);
+
+    setTimeout(function() {
+        squares.forEach((square) => {
+            square.classList.add("hidden");
+            square.addEventListener("click", showColor);
+        });
+    }, 2000);
 };
 
-startGame();
+button.addEventListener("click", startGame);
